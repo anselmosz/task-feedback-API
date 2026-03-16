1b1968ef01245d5fe7d562fe332c49a531bba2e0
anselmosz <anselmoma2005@outlook.com>
2026-03-16 12:07:45 -0300

    chore: Atualizados métodos de controle de erro e classe de construção de mensagem de erro padronizada

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -3,5 +3,7 @@ export class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
+  
+    Error.captureStackTrace(this, this.constructor);
   }
 }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,4 +4,4 @@ export const asyncHandler = (fn) => {
   return (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   }
-}
+};
