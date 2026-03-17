7d254d73b1c86b3ea230a047617ce29e02dad8bf
anselmosz <anselmoma2005@outlook.com>
2026-03-17 11:11:37 -0300

    feat(surveys): Adicionada rota de surveys em app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-import express from 'express'
-import cors from 'cors';
-import userRoutes from './modules/users/users.routes.js';
-import authRoutes from './modules/auth/auth.routes.js';
+import express from "express";
+import cors from "cors";
+import userRoutes from "./modules/users/users.routes.js";
+import authRoutes from "./modules/auth/auth.routes.js";
+import surveyRoutes from "./modules/surveys/survey.routes.js";
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 // Rotas
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
+app.use("/surveys",surveyRoutes);
 
 export default app;
